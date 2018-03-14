7f1f5d3e8936262547ad5e13dcf11e0b7f9c918e
tony <narifu@163.com>
2018-03-14 23:01:01 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,28 @@ $(document).ready(function(){
             dayViewHeaderFormat:"YYYY年 MMMM"
         });
     }
+    var mobileSelect1 = new MobileSelect({
+        trigger: '#trigger1',
+        title: '请选择胎次',
+        wheels: [
+                    {data:['1','2','3','4']}
+                ],
+        position:[2] //初始化定位
+    });
+    var mobileSelect2 = new MobileSelect({
+        trigger: '#trigger2',
+        title: '请选择县市',
+        wheels: [
+                    {data:['中部','南部','北部']}
+                ], 
+        position:[2] //初始化定位
+    });
+    var mobileSelect3 = new MobileSelect({
+        trigger: '#trigger3',
+        title: '请选择区',
+        wheels: [
+                    {data:['中正区','大同区','松山区','内湖区']}
+                ],
+        position:[2] //初始化定位
+    });
 })
